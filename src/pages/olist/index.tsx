8d44fe6d6065f50d70327ac3218621407b11ebdb
gaoshaoyun <276185876@qq.com>
2020-07-10 16:42:13 +0800

    订单列表

--- src/pages/olist/index.tsx
+++ src/pages/olist/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import styles from './index.less';
+import React, { Component } from 'react';
+import { WingBlank, WhiteSpace } from 'antd-mobile';
+import { query } from '@/services/olist';
+import { CartProductType } from '@/@types/product';
+import List from './List/index';
 
-export default () => {
-  return (
-    <div>
-      <h1 className={styles.title}>Page olist/index</h1>
-    </div>
-  );
-};
+export interface OListState {
+  data: CartProductType[];
+}
+
+class OList extends Component<{}, OListState> {
+  state: OListState = {
+    data: [],
+  };
+  componentDidMount() {
+    query().then(res => {
+      this.setState({ data: res.list.data });
+    });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <WingBlank size="lg">
+        <WhiteSpace size="lg" />
+        <List data={data} />
+      </WingBlank>
+    );
+  }
+}
+
+export default OList;
